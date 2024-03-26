5f1debe46017886128f920af9641f1f7bbe35811
WaseemAbbasi22 <waseemabbasi480@gmail.com>
2024-03-27 03:07:09 +0400

    add scanner service

--- lib/src/mrz_helper.dart
+++ lib/src/mrz_helper.dart
@@ -19,13 +19,26 @@ class MRZHelper {
     }
     return null;
   }
-
   static String testTextLine(String text) {
     String res = text.replaceAll(' ', '');
     List<String> list = res.split('');
+    List<String> updatedMRZLines = [];
 
+    for (String line in list) {
+      if (line.startsWith("P") && line.endsWith("<")) {
+        // Line starts with "P" and ends with "<"
+        if (line.length < 44) {
+          // Length is less than 44, append "<" characters to make it 44
+          line = line.padRight(44, "<");
+        }
+      }
+      updatedMRZLines.add(line);
+    }
+    if(updatedMRZLines.isNotEmpty){
+      list = updatedMRZLines;
+    }
     // to check if the text belongs to any MRZ format or not
-    if (list.length != 44 && list.length != 30 && list.length != 36) {
+    if (list.length != 44) {
       return '';
     }
 
@@ -43,4 +56,28 @@ class MRZHelper {
     String result = list.join('');
     return result;
   }
+
+  // static String testTextLine(String text) {
+  //   String res = text.replaceAll(' ', '');
+  //   List<String> list = res.split('');
+  //
+  //   // to check if the text belongs to any MRZ format or not
+  //   if (list.length != 44 && list.length != 30 && list.length != 36) {
+  //     return '';
+  //   }
+  //
+  //   for (int i = 0; i < list.length; i++) {
+  //     if (RegExp(r'^[A-Za-z0-9_.]+$').hasMatch(list[i])) {
+  //       list[i] = list[i].toUpperCase();
+  //       // to ensure that every letter is uppercase
+  //     }
+  //     if (double.tryParse(list[i]) == null &&
+  //         !(RegExp(r'^[A-Za-z0-9_.]+$').hasMatch(list[i]))) {
+  //       list[i] = '<';
+  //       // sometimes < sign not recognized well
+  //     }
+  //   }
+  //   String result = list.join('');
+  //   return result;
+  // }
 }
