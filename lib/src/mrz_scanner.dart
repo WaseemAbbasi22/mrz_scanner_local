e8d65a682689f714838c7a8965ed98e5a975fc98
WaseemAbbasi22 <waseemabbasi480@gmail.com>
2024-03-27 22:55:14 +0400

    add services class

--- lib/src/mrz_scanner.dart
+++ lib/src/mrz_scanner.dart
@@ -91,34 +91,4 @@ class MRZScannerState extends State<MRZScanner> {
 
 }
 
-class MRZScannerService {
-  MRZScannerService();
-  final TextRecognizer _textRecognizer = TextRecognizer();
 
-  Future<MRZResult?> processImage(File image) async {
-    try {
-      InputImage inputImg = InputImage.fromFile(image);
-      await _textRecognizer.processImage(inputImg);
-      final recognisedText =await _textRecognizer.processImage(inputImg);
-      String fullText = recognisedText.text;
-      String trimmedText = fullText.replaceAll(' ', '');
-      List allText = trimmedText.split('\n');
-      List<String> ableToScanText = [];
-      for (var e in allText) {
-        if (MRZHelper.testTextLine(e).isNotEmpty) {
-          ableToScanText.add(MRZHelper.testTextLine(e));
-        }
-      }
-      List<String>? result = MRZHelper.getFinalListToParse([...ableToScanText]);
-      if (result != null) {
-
-        return MRZParser.parse(result);
-      } else {
-        return null;
-      }
-    } catch (e) {
-      print('Error processing image: $e');
-      return null;
-    }
-  }
-}
