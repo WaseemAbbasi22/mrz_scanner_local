5609a5b8f76ceeed857b7f08e74ef714d9ded720
WaseemAbbasi22 <waseemabbasi480@gmail.com>
2024-03-27 23:09:27 +0400

    add imageparser method in helperclass

--- lib/mrz_scanner.dart
+++ lib/mrz_scanner.dart
@@ -3,6 +3,5 @@ library mrz_scanner;
 
 export 'src/mrz_scanner.dart';
 export 'src/mrz_controller.dart';
-export 'src/mrz_services.dart';
 export 'package:mrz_parser/mrz_parser.dart';
 export 'package:camera_platform_interface/src/types/camera_description.dart';
--- lib/src/mrz_helper.dart
+++ lib/src/mrz_helper.dart
@@ -1,4 +1,37 @@
+import 'dart:io';
+
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:mrz_parser/mrz_parser.dart';
+
 class MRZHelper {
+
+  static Future<MRZResult?> processImage(File image) async {
+    try {
+      final TextRecognizer _textRecognizer = TextRecognizer();
+      InputImage inputImg = InputImage.fromFile(image);
+      await _textRecognizer.processImage(inputImg);
+      final recognisedText =await _textRecognizer.processImage(inputImg);
+      String fullText = recognisedText.text;
+      String trimmedText = fullText.replaceAll(' ', '');
+      List allText = trimmedText.split('\n');
+      List<String> ableToScanText = [];
+      for (var e in allText) {
+        if (MRZHelper.testTextLine(e).isNotEmpty) {
+          ableToScanText.add(MRZHelper.testTextLine(e));
+        }
+      }
+      List<String>? result = MRZHelper.getFinalListToParse([...ableToScanText]);
+      if (result != null) {
+
+        return MRZParser.parse(result);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error processing image: $e');
+      return null;
+    }
+  }
   static List<String>? getFinalListToParse(List<String> ableToScanTextList) {
     if (ableToScanTextList.length < 2) {
       // minimum length of any MRZ format is 2 lines
